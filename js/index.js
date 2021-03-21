a04a9069fceda93f596f303245ae8e318dba17eb
jameshmwangi <jameshmwangiy@gmail.com>
2021-03-21 20:48:17 +0300

    Staging

---
+++ js/index.js
@@ -0,0 +1,11 @@
+
+function change_text(){
+    
+    document.getElementById("greet").innerHTML = "Jambo, mimi ni Kamau-James" ;
+    document.getElementById("para1").innerHTML ="Ninadhamini jamii za wasanidu programu. Jamii hizi zilinipa nafasi ya kutandaza na kukua kama msanidu wa  tovuti. Mimi ni mwanachama wa DSC JKUAT, MongoDB Nairobi na MSP JKUAT. Mimi na rafiki zangu tuliunda Kuku Empire Coding nights zinazofanyika kila Ijumaa.";
+    
+
+    
+    alert("Page will be translated, Click on the back button to Reverse");
+}
+
